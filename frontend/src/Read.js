646ba62600ecf87a562d41dbea0028659292d194
boureimatapily <boureimatapily1@gmail.com>
2023-05-18 19:10:42 +0000

    Add read.js

--- frontend/src/Read.js
+++ frontend/src/Read.js
@@ -1,18 +1,36 @@
-import axios from 'axios';
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 function Read() {
-    const {id} = useParams();
-    useEffect(() => {
-        axios
-          .get("http://localhost:8081/read/"+id)
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err));
-      }, []);
+  const { id } = useParams();
+  const [student, setStudent] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8081/read/'+id)
+      .then(res => {
+        console.log(res)
+        setStudent(res.data[0])
+    })
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
-    <div>Read</div>
-  )
+    <>
+      <div className="d-flex vh-100 bg-secondary justify-content-center align-items-center">
+        <div className="w-50 bg-white rounded p-1">
+          <div className="p-2">
+          <h2>Person Detail</h2>
+          <h2> {student.ID} </h2>
+          <h2> {student.Name} </h2>
+          <h2> {student.Email} </h2>
+          </div>
+          <Link to="/" className="btn btn-primary me-2">Back</Link>
+          <button  className="btn btn-primary">Edit</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Read
+export default Read;
